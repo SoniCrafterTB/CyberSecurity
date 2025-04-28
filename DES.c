8ab423942df372f8438599780f56f603921cfeb7
SoniCrafterTB <37859972+SoniCrafterTB@users.noreply.github.com>
2025-04-28 09:21:23 +0300

    Add files via upload

--- DES.c
+++ DES.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <openssl/des.h>
-#define key_size 8
-#define MAXN 100
+#define KEY_SIZE 8
+#define MAXN 1000
 
 int main()
 {
-    unsigned char key[key_size] = "Banichka";
+    unsigned char key[KEY_SIZE] = "banichka";
     unsigned char input[MAXN];
 
-    fgets((char*)input,MAXN,stdin);
-    input[strcspn(input,"\n")]='\0';
+    scanf("%s", input);
+
     unsigned int length = strlen(input);
-    unsigned int padded_length = ((length +7)/8)*8;
+
+    int padded_length = ((length + 7) / 8) * 8;
     unsigned char padded_input[padded_length];
     unsigned char output[padded_length];
 
-    memcpy(padded_input,input,length);
-    memset(padded_input+length,padded_length-length,padded_length-length);
+    int difference_length = padded_length - length;
+    memcpy(padded_input, input, length);
+    memset(padded_input + length, difference_length, difference_length);
 
     DES_key_schedule key_schedule;
     DES_set_key( (DES_cblock*) key, &key_schedule);
 
-    for (int i = 0; i < padded_length; i++)
+    for(int i = 0; i < padded_length; i += 8)
     {
-        DES_ecb_encrypt((DES_cblock*) input, (DES_cblock*) output, &key_schedule, 
-    DES_ENCRYPT);
+        DES_ecb_encrypt( (DES_cblock*) (padded_input + i),
+            (DES_cblock*) (output + i), &key_schedule, DES_ENCRYPT);
     }
-    
-    for(int i = 0; i < length; i++)
+
+    for(int i = 0; i < padded_length; i++)
     {
         printf("%02x", output[i]);
     }
-
     return EXIT_SUCCESS;
 }
-
-    
-
-
